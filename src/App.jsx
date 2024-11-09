0fd27adde311e85764bfdcd1074204832b5c2815
Arthur-ceap <arthurg2552@gmail.com>
2024-11-09 18:22:17 -0300

    subindo arquivos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { TodoWrapper } from './Components/ToDoWrapper';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoWrapper  />
+    </div>
+  );
+}
+
+export default App;
